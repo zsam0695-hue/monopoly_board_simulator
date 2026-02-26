f3c93a407429617c1a9e30eaba07c2fb8a8fb1d3
Zahra <zsam0695@sdsu.edu>
2026-02-25 16:22:40 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+static const int MAX_SPACES = 40;
+// -------------------------------
+// Data class (NOT a struct)
+// -------------------------------
+class MonopolySpace {
+
+};
